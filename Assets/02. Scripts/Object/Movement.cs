79e53a3a01c0da2663fc03b56383db06f0f6652b
young <seoyoung1025s@gmail.com>
2025-05-23 17:36:18 +0900

    [ADD] GameObject Upgrade, New Object
    2025. 05. 23. 금요일
    -- GetAxisRaw 구현
    -- LookAt 구현
    -- Awake 구현
    -- 총 쏘는 포탑 Object 추가
    -- 태양계를 통한 Rotation 복습

--- Assets/02. Scripts/Object/Movement.cs
+++ Assets/02. Scripts/Object/Movement.cs
@@ -13,12 +13,21 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("Horizontal"); // 수평 Z의 값
-        float v = Input.GetAxis("Vertical"); // 수직 X의 값
+        //float h = Input.GetAxis("Horizontal"); // 수평 Z의 값
+        //float v = Input.GetAxis("Vertical"); // 수직 X의 값
 
-        Vector3 dir = new Vector3(h, 0, v);
+        float h = Input.GetAxisRaw("Horizontal"); // 수평 Z의 값
+        float v = Input.GetAxisRaw("Vertical"); // 수직 X의 값
+
+        Vector3 dir = new Vector3(h, 0, v).normalized;
+        //Vector3 dir = new Vector3(h, 0, v);
+        //Vector3 normalDir = dir.normalized; // 정규화 과정 (0 ~ 1)
+
+
         Debug.Log($"현재 입력 : {dir}");
 
         transform.position += dir * moveSpeed * Time.deltaTime;
+
+        transform.LookAt(transform.position + dir);
     }
 }
--- Assets/02. Scripts/Object/StudyGameObject.cs
+++ Assets/02. Scripts/Object/StudyGameObject.cs
@@ -7,7 +7,7 @@ public class StudyGameObject : MonoBehaviour
     //public Vector3 pos;
     //public Quaternion rot;
 
-    void Start()
+    void Awake()
     {
         CreateREPO();
     }
